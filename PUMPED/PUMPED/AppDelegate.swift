79e65f5abf72e70d1afd18f552fcd1bace69863c
jnguyen113 <jnguyen113@users.noreply.github.com>
2020-05-19 13:32:22 -0400

    without firebase

--- PUMPED/PUMPED/AppDelegate.swift
+++ PUMPED/PUMPED/AppDelegate.swift
@@ -7,7 +7,7 @@
 //
 
 import UIKit
-import Firebase
+
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -16,8 +16,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
-        FirebaseApp.configure()
-        FIRApp.configure()
+
         return true
     }
 
