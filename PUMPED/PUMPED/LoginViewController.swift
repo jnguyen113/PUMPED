18833ccac2b39fe2014cd0de4586cb6f82003086
jnguyen113 <jnguyen113@users.noreply.github.com>
2020-05-20 12:31:37 -0400

    login page with firebase

---
+++ PUMPED/PUMPED/LoginViewController.swift
@@ -0,0 +1,69 @@
+//
+//  LoginViewController.swift
+//  PUMPED
+//
+//  Created by Johnathan Nguyen on 5/19/20.
+//  Copyright © 2020 Johnathan Nguyen. All rights reserved.
+//
+
+import UIKit
+import FirebaseAuth
+
+class LoginViewController: UIViewController {
+    @IBOutlet weak var Email: UITextField!
+    @IBOutlet weak var Password: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        checkUserInfo()
+    }
+
+    @IBAction func loginTapped(_ sender: Any) {
+            validateFields()
+    }
+    
+    
+    
+    @IBAction func createAccountTap(_ sender: Any) {
+        let storyboard = UIStoryboard(name: "Main" ,bundle: nil)
+        let vc = storyboard.instantiateViewController(withIdentifier: "signUp")
+        vc.modalPresentationStyle = .overFullScreen
+        present(vc, animated: true)
+    }
+    
+    func validateFields() {
+        if Email.text?.isEmpty == true {
+            print("No Email Text")
+            return
+        }
+        if Password.text?.isEmpty == true {
+            print ("No Password")
+            return
+        }
+        login()
+    }
+    
+    func login() {
+        Auth.auth().signIn(withEmail: Email.text!, password: Password.text!) { [weak self] authResult, err in
+            guard let strongSelf = self else {return}
+            if let err = err {
+                print(err.localizedDescription)//so we know what error
+            }
+            self!.checkUserInfo()
+        }
+    }
+    func checkUserInfo() {
+        if Auth.auth().currentUser != nil {
+            print(Auth.auth().currentUser?.uid)
+            let storyboard = UIStoryboard(name: "Main" ,bundle: nil)
+            let vc = storyboard.instantiateViewController(withIdentifier: "homePage")
+            vc.modalPresentationStyle = .overFullScreen
+            present(vc, animated: true)
+        }
+    }
+}
---
+++ PUMPED/PUMPED/SignUpViewController.swift
@@ -0,0 +1,71 @@
+//
+//  SignUpViewController.swift
+//  PUMPED
+//
+//  Created by Johnathan Nguyen on 5/19/20.
+//  Copyright © 2020 Johnathan Nguyen. All rights reserved.
+//
+
+import UIKit
+import FirebaseAuth
+import Firebase
+
+class SignUpViewController: UIViewController {
+
+    @IBOutlet weak var Email: UITextField!
+    @IBOutlet weak var Password: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    @IBAction func SignUpTap(_ sender: Any) {
+        if Email.text?.isEmpty == true {
+                print("No text in email field")
+                return
+        }
+        if Password.text?.isEmpty == true {
+                print("No text in password field")
+                return
+        }
+        signUP()
+    }
+    
+
+    
+    @IBAction func alreadyHaveAccountTap(_ sender: Any) {
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        let vc = self.storyboard?.instantiateViewController(withIdentifier: "Login")
+        vc?.modalPresentationStyle = .overFullScreen
+        present(vc!, animated: true)
+    }
+    
+    func signUP(){
+        Auth.auth().createUser(withEmail: Email.text!, password: Password.text!) {
+            (authResult, error) in
+            guard let user = authResult?.user, error == nil else {
+                print("Error \(error?.localizedDescription)")
+                return
+            }
+            let storyboard = UIStoryboard(name: "Main", bundle: nil)
+            let vc = self.storyboard?.instantiateViewController(withIdentifier: "homePage")
+            vc?.modalPresentationStyle = .overFullScreen
+            self.present(vc!, animated: true)
+        }
+    }
+//    @IBAction func signUP(_ sender: Any) {
+//        Auth.auth().createUser(withEmail: Email.text!, password: Password.text!) { (authResult, error) in
+//                guard let user = authResult?.user, error == nil else{
+//                    print("Error \(error?.localizedDescription)")
+//                    return
+//                }
+//                let sb = UIStoryboard(name: "Main", bundle: nil)
+//                let vc = self.storyboard?.instantiateViewController(withIdentifier: "mainHome")
+//                vc?.modalPresentationStyle = .overFullScreen
+//                self.present(vc!, animated: true)
+//        }
+//    }
+}
